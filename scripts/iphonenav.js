26ec94e34880c99dbf4870b0571dae26c94e3dbf
Ramon Wijnands <ramon.wijnands007@gmail.com>
2009-10-29 21:36:25 +0100

    Move onLoad code to DomReady

--- scripts/iphonenav.js
+++ scripts/iphonenav.js
@@ -19,35 +19,19 @@ var currentHash = location.hash;
 var hashPrefix = "#_";
 var pageHistory = [];
 
-addEventListener("load", function(event)
-{
-	// Get all elements with selected = true (there is no other way)
-	list = $(document.body).getChildren().filter(function(el){
-		return el.getAttribute('selected')
-	});
-	//Startpage is the first selected = true, pick the fist div when no elements are found
-	startPage = list.length > 0 ? list[0] : $(document.body).getElement('div');
-	//Show the startpage
-	showPage(startPage);
-	
-	//Add some time stuff
-    setInterval(checkOrientAndLocation, 300);
-    setTimeout(scrollTo, 0, 0, 1);
-}, false);
-    
 addEventListener("click", function(event)
 {
     var link = event.target;
     // Search the <a> tag
     while (link && link.localName && link.localName.toLowerCase() != "a")
         link = link.parentNode;
-    
+
     // Dont do anything with normal links
     if (link && link.hash && link.hash != '' && link.hash != '#')
     {
 		//Stop default action
 		event.preventDefault();
-		
+
 		//Get the page, and when found, go to it
 		var page = document.getElementById(link.hash.substr(1));
 		if(page)
@@ -82,7 +66,7 @@ function checkOrientAndLocation()
             var backwards = index != -1;
             if (backwards)
                 pageHistory.splice(index, pageHistory.length);
-                
+
             showPage(page, backwards);
         }
         else
@@ -91,9 +75,9 @@ function checkOrientAndLocation()
 		}
     }
 }
-    
+
 function showPage(page, backwards)
-{    
+{
     // If classname == dialog means that it is a form
     if (page.className.indexOf("dialog") != -1)
         showDialog(page);
@@ -118,11 +102,11 @@ function showPage(page, backwards)
 }
 
 function swipePage(fromPage, toPage, backwards)
-{        
+{
     toPage.style.left = "100%";
     toPage.setAttribute("selected", "true");
     scrollTo(0, 1);
-    
+
     var percent = 100;
     var timer = setInterval(function()
     {
@@ -134,8 +118,8 @@ function swipePage(fromPage, toPage, backwards)
             clearInterval(timer);
         }
 
-        fromPage.style.left = (backwards ? (100-percent) : (percent-100)) + "%"; 
-        toPage.style.left = (backwards ? -percent : percent) + "%"; 
+        fromPage.style.left = (backwards ? (100-percent) : (percent-100)) + "%";
+        toPage.style.left = (backwards ? -percent : percent) + "%";
     }, animateInterval);
 }
 
@@ -146,20 +130,34 @@ function showDialog(form)
 
 window.addEvent('domready', function() {
 
+	// Get all elements with selected = true
+	list = $(document.body).getChildren().filter(function(el){
+		return el.getAttribute('selected')
+	});
+
+	//Startpage is the first selected = true, pick the fist div when no elements are found
+	startPage = list.length > 0 ? list[0] : $(document.body).getElement('div');
+	//Show the startpage
+	showPage(startPage);
+
+	//Add some time stuff
+    setInterval(checkOrientAndLocation, 300);
+    setTimeout(scrollTo, 0, 0, 1);
+
 	//Add some events to forms with class=dialog
     $$('[class=dialog]').addEvents({
-    
-		//Add an onsubmit event handler to all forms that have class=dialog   
+
+		//Add an onsubmit event handler to all forms that have class=dialog
 		'submit': function(event){
-			
+
 			//Stop the submitting of the form
 			event.preventDefault();
-			
+
 			// Deselect all inputs
 			this.getElements('input').each(function(item){
 				item.blur();
 			});
-			
+
 			//Hide the form
 			this.removeAttribute("selected");
 
@@ -170,7 +168,7 @@ window.addEvent('domready', function() {
 				//Get the submit location
 				var element = document.getElementById(this.action.substr(index+1));
 				if(element)
-				{		
+				{
 					showPage(element);
 				}
 				else
@@ -178,15 +176,15 @@ window.addEvent('domready', function() {
 					alert('Page not found');
 				}
 			}
-			
+
 		},
-		
-		//Hide the form when an click event occurs 
+
+		//Hide the form when an click event occurs
 		'click': function(event){
 			if (event.target == this)
 				this.removeAttribute("selected");
 		}
-	});	
+	});
 });
 
 
