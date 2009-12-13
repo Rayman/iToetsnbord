455d38aab035c9f9423cc5d98f3d7eaf19806fdc
Ramon Wijnands <ramon.wijnands007@gmail.com>
2009-12-13 21:09:25 +0100

    Fixed the current song displaying

--- scripts/iphoneinit2.js
+++ scripts/iphoneinit2.js
@@ -8,3 +8,144 @@ var currentSongPlaying = null;
 //Public vars
 var currentSongManager;
 var searcher;
+
+window.addEvent('domready', function() {
+	//Some vars
+	var currentTitle 	= $('currentTitle');
+	var currentArtist 	= $('currentArtist');
+	var artistLink 		= currentArtist.getParent();
+	var currentAlbum 	= $('currentAlbum');
+	var albumLink 		= currentAlbum.getParent();
+	var currentAlbumArt = $('currentAlbumArt');
+	var currentInfo 	= $('currentInfo');
+	var infoLink 	 	= currentInfo.getParent();
+
+	var optionsShuffle 	= $('optionsShuffle');
+	var optionsRepeat 	= $('optionsRepeat');
+	var optionsLock 	= $('optionsLock');
+
+	var loadingImage = new Element('img',{
+		src: 'images/loading.gif'
+	});
+
+	//Init the song manager
+	currentSongManager = new SongManager({
+		baseUrl: 'json/getcurrent.html',
+		onUpdateStart: function(){
+			Log.log('update start');
+
+			$$(currentTitle,
+				currentArtist,
+				currentAlbum,
+				currentInfo
+			).each(function(el){
+				el.empty();
+				loadingImage.clone().inject(el);
+			});
+		},
+		onUpdate: function(responseJSON){
+			Log.log('currentSongManager onupdate: ',responseJSON);
+
+			currentTitle.set('html',responseJSON.title);
+			currentArtist.set('html',responseJSON.artist);
+			currentAlbum.set('html',responseJSON.album);
+			var currentWidth = currentAlbumArt
+				.set('width')
+				.set('src',responseJSON.albumart)
+				.get('width');
+			if(currentWidth > 200)
+				currentAlbumArt.set('width',200);
+
+			currentInfo.set('html',
+				responseJSON.length + ", " +
+				responseJSON.bitrate + "kbps, " +
+				responseJSON.samplerate + "kHz " +
+				responseJSON.channels
+			);
+		}
+	});
+
+	//When the play controls are clicked, get the html page and update the current song
+	$('playControls').getElements('a').each(function(el){
+			el.addEvent('click', function(event){
+			event.preventDefault();
+			currentSongManager.update(el.get('href'));
+		});
+	});
+
+	//Init the searcher
+	searcher = new MusicSearcher({
+		//The url for the searches by query
+		getSearchResultsUrl: 'json/getsearchresults.html?query=',
+
+		//The url for searches by keyword
+		getSearchByKeyUrl:   'json/getsearchresults.html?search_ml=',
+
+		onSearchStart: function(query){
+			Log.log('query: ',query);
+		},
+
+		//When the search request is complete
+		onSearchComplete: function(responseJSON, responseText){
+			Log.log('Searchcomplete: ',responseJSON);
+		}
+	});
+
+	//Add listener for album searching
+	currentAlbum.addEvent('click',function(){
+		searcher.search('ALBUM HAS "'+currentSongPlaying.album+'"');
+	});
+
+	//Add listener for artist searching
+	currentArtist.addEvent('click',function(){
+		searcher.search('ARTIST HAS "'+currentSongPlaying.artist+'"');
+	});
+
+	//Add listener for search form submit
+	$('searchBySong').addEvent('submit',function(e){
+		//Get the artist and song value
+		var artist = this.getElement('input[name=artist]').value.trim();
+		var song   = this.getElement('input[name=song]').value.trim();
+
+		//Format the query
+		artist = artist == '' ? '' : 'ARTIST HAS "' + artist +'"';
+		song   = song   == '' ? '' : 'TITLE HAS  "' + song   +'"';
+		query = (song != '' && artist != '') ? song + ' && ' + artist : song + artist;
+
+		//Query valid?
+		if(!searcher.search(query))
+			searcher.fireEvent('searchComplete', [{}, "Empty Query"]);
+	});
+
+	//Add listener for search by query
+	$('searchByQuery').addEvent('submit',function(e){
+		//query valid?
+		if(!searcher.search(this.getElement('input[name=query]').value.trim()))
+			searcher.fireEvent('searchComplete', [{}, "Empty Query"]);
+	});
+
+	//Add listener for search by keyword
+	$('searchByKeyword').addEvent('submit',function(e){
+		//query valid?
+		if(!searcher.searchByKey(this.getElement('input[name=key]').value.trim()))
+			searcher.fireEvent('searchComplete', [{}, "No Key Specified"]);
+	});
+
+	//When user clicks the link to options, we do a quick request of the variables
+	$('getOptions').addEvent('click', function(){
+		currentSongManager.update();
+	});
+
+	//Listener for options
+	$('options').getElements('a').each(function(el){
+		el.addEvent('click', function(event){
+			event.preventDefault();
+			currentSongManager.update(el.get('href'));
+		});
+	});
+
+	//UpDATE!!!
+	currentSongManager.update();
+});
+
+//End of file!
--- scripts/itoetsnbord2.js
+++ scripts/itoetsnbord2.js
@@ -32,3 +32,62 @@ var DataManager = new Class({
 		this.fireEvent('onUpdateStart');
 	}
 });
+
+var SongManager = new Class({
+
+	Extends: DataManager,
+
+	options: {
+		// onUpdateStart: $empty,
+		// onUpdate: $empty,
+		// baseUrl: ''
+	},
+
+	initialize: function(options) {
+		this.parent(options);
+	},
+
+	//Get the current song with an ajax request and fire the update event
+	update: function(data){
+		this.parent(data);
+	}
+});
+
+var MusicSearcher = new Class({
+
+	Extends: DataManager,
+
+	options: {
+		// onUpdateStart: $empty,
+		// onUpdate: $empty,
+		// baseUrl: ''
+	},
+
+	initialize: function(options) {
+		//set options
+		this.parent(options);
+
+		//Set some options on the request instance
+		this.xhr.setOptions({
+			onSuccess: function(responseJSON, responseText){
+				this.fireEvent('searchComplete', [responseJSON, responseText]);
+			}.bind(this)
+		});
+	},
+
+	//Query the media library with ajax request and fire the event
+	searchByQuery: function(query){
+		this.fireEvent('searchStart', query);
+		if(!$chk(query)) return false;
+		this.parent(query);
+		return true;
+	},
+
+	//Search in the media library for this key
+	searchByKey: function(key){
+		this.fireEvent('searchStart', 'Search for: ' + key);
+		if(!$chk(key)) return false;
+		this.parent(key);
+		return true;
+	}
+});
