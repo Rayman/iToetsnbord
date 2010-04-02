96d878c0a82cfaf5a6d8c933b42f9103335317eb
Ramon Wijnands <ramon.wijnands007@gmail.com>
2010-04-02 15:37:37 +0200

    Removed iPhoneNav class, made an anonymous function of it

--- scripts/iwebkit.js
+++ scripts/iwebkit.js
@@ -1,4 +1,4 @@
-function fullscreen() {
+/* function fullscreen() {
 	var a = document.getElementsByTagName("a");
 	for (var i = 0; i < a.length;i++) {
 		if (a[i].className.match("noeffect")) {
@@ -10,14 +10,14 @@ function fullscreen() {
 			};
 		}
 	}
-}
+} */
 
 function hideURLbar() {
 	window.scrollTo(0, 0.9);
 }
 
 window.onload = function () {
-	fullscreen();
+	// fullscreen();
 	hideURLbar();
 };
 
@@ -26,147 +26,24 @@ document.addEventListener('click', function(){
   return false;
 },false); */
 
-log('Start iPhoneNav');
-var iPhoneNav = new Class({
-
-	Implements: [Options],
-
-	options: {
-		observeDelay:	300,
-		hashPrefix:   "#_", //The pageId gets prefixed in the url by this
-		backButton:   '', //This button is hidden when page.id == homepage.id
-		homePage:     ''
-	},
-
-	initialize: function (options) {
-		this.setOptions(options);
-
-		this.currentWidth = 0;
-		this.currentHash = location.hash;
-		this.pageHistory = [];
-
-		//Start the clickwatcher
-		window.addEventListener('click', this.onClick.bind(this));
-	},
-
-	onClick: function (event) {
-		var link = event.target;
-
-		//Hide the form if it's clicked on it
-		if (link.nodeName.toLowerCase() === 'form') {
-			link.removeAttribute("selected");
-		}
-
-		// Search the <a> tag
-		while (link && link.localName && link.localName.toLowerCase() !== "a") {
-			link = link.parentNode;
-		}
-
-		// Dont do anything with normal links
-		if (link && link.hash && link.hash !== '' && link.hash !== '#')
-		{
-			//Stop default action
-			event.preventDefault();
-
-			this.goToPage(link.hash.substring(1));
-		}
-	},
-
-	goToPage: function (pageId) {
-		var page = this.tryGetPage(pageId);
-		if (page) {
-			this.showPage(page);
-		}
-	},
-
-	showPage: function (page, backwards) {
-
-		// If classname == dialog means that it is a form
-		if (hasClass(page, 'content')) {
-			this.showDialog(page);
-		} else {
-			//Change the location to the page that about to be shown
-			location.href = this.currentHash = this.options.hashPrefix + page.id;
-
-			//Save the page in the history
-			this.pageHistory.push(page.id);
-
-			var fromPage = this.currentPage;
-			this.currentPage = page;
-
-			//Set the title
-			$('title').innerHTML = page.title || "";
-
-			//Hide the backButton when page === home
-			this.options.backButton.style.display = page === this.options.homePage ? "none" : "inline";
-
-			if (fromPage && fromPage !== this.currentPage) {
-				setTimeout(this.swipePage.bind(this), 0, fromPage, page, backwards);
-			}
-		}
-	},
-
-	showDialog: function (form) {
-		//Unhide the form
-		form.setAttribute("selected", "true");
-
-		//Remove the old query
-		form.getElementsByTagName('input').value = '';
-	},
-
-	tryGetPage: function (pageId) {
-		var page = $(pageId);
-		if (!page) {
-			alert('Page not found: ' + pageId);
-		}
-		return page;
-	},
-
-	timeoutID: null,
-
-	swipePage: function (fromPage, toPage, backwards)
-	{
-		//Stop the other page from hiding
-		$clear(this.timeoutID);
-
-		// position the toPage right next to the current page
-		toPage.setStyle('left', backwards ? '-100%' : '100%');
-
-		//Unhide it
-		toPage.setAttribute("selected", "true");
-
-		//Scroll to the top
-		scrollTo(0, 1);
-
-		fromPage.setStyle('left', (backwards ? '100' : '-100') + "%");
-		toPage.setStyle('left', '0%');
-
-		this.timeoutID = function () {
-			fromPage.removeAttribute("selected"); //Hide the fromPage
-		}.delay(1000);
-	}
-});
-
 window.addEventListener('load', function () {
 
-	// Get all elements with class selected
-	var list = document.getElementsByClassName('selected');
-	
-	if(list.length == 0) alert('Error, no startpage set');
-
-	//Startpage is the first selected = true, pick the fist div when no elements are found
+  // Get all elements with class selected
+	var list = document.getElementsByClassName('selected');	
+	if(list.length !== 1) alert('Error, no startpage or too many startpages');
+	//Startpage is the first selected = true
 	var startPage = list[0];
 
-	//Set some options
-	var HistoryManager = new iPhoneNav({
-		homePage:	startPage,
-		backButton:	$('backButton')
-	});
-
-	//Show the startpage
-	HistoryManager.showPage(startPage);
+  var	observeDelay  = 300,
+      backButton    = 	$('backButton'), //This button is hidden when page.id == homepage.id
+      homePage      = startPage,
+      pageHistory   = [],
+      currentPage;
+      
+  showPage(startPage);
 
 	//Bind the events for form submitting
+	/*
 	var elements = document.getElementsByClassName('dialog');
 	var i = 0;
 	for(;i<elements.length;i++){
@@ -185,7 +62,7 @@ window.addEventListener('load', function () {
       });
 
       //Hide the form
-      form.removeAttribute("selected");
+      removeClass(form, 'selected');
 
       //Check if the action is to a #id
       var index = form.action.lastIndexOf("#");
@@ -195,5 +72,106 @@ window.addEventListener('load', function () {
         HistoryManager.goToPage(form.action.substr(index + 1));
       }
     });
-	}
-});
+	} */
+
+  //Start the clickwatcher
+  window.addEventListener('click', onClick);
+
+  function 	onClick(event) {
+    var link = event.target;
+
+    //Hide the form if it's clicked on it
+    if (link.nodeName.toLowerCase() === 'form') {
+      removeClass(link, 'selected');
+    }
+
+    // Search the <a> tag
+    while (link && link.localName && link.localName.toLowerCase() !== "a") {
+      link = link.parentNode;
+    }
+
+    // Dont do anything with normal links
+    if (link && link.hash && link.hash !== '' && link.hash !== '#')
+    {
+      //Stop default action
+      event.preventDefault();
+
+      goToPage(link.hash.substring(1));
+    }
+  }
+
+  function goToPage(pageId) {
+    var page = tryGetPage(pageId);
+    if (page) {
+      showPage(page);
+    }
+  }
+
+  function showPage(page, backwards) {
+    log('showPage', page);
+
+    // If classname == dialog means that it is a form
+    if (hasClass(page, 'dialog')) {
+      showDialog(page);
+    } else {
+      //Change the location to the page that about to be shown
+      location.href = currentHash = '#' + page.id;
+
+      //Save the page in the history
+      pageHistory.push(page.id);
+
+      var fromPage = currentPage;
+      currentPage = page;
+
+      //Set the title
+      $('title').innerHTML = page.title || "";
+
+      //Hide the backButton when page === home
+      backButton.style.display = page === homePage ? "none" : "inline";
+
+      if (fromPage && fromPage !== currentPage) {
+        setTimeout(swipePage, 0, fromPage, page, backwards);
+      }
+    }
+  }
+
+  function showDialog(form) {
+    //Unhide the form
+    addClass(form, 'selected');
+
+    //Remove the old query
+    form.getElementsByTagName('input').value = '';
+  }
+
+  function tryGetPage(pageId) {
+    var page = $(pageId);
+    if (!page) {
+      alert('Page not found: ' + pageId);
+    }
+    return page;
+  }
+
+  var timeoutID = null;
+
+  function swipePage(fromPage, toPage, backwards)	{
+    //Stop the other page from hiding
+    clearTimeout(timeoutID);
+
+    // position the toPage right next to the current page
+    toPage.style.left = backwards ? '-100%' : '100%';
+
+    //Unhide it
+    addClass(toPage, 'selected');
+
+    //Scroll to the top
+    scrollTo(0, 1);
+
+    fromPage.style.left = (backwards ? '100' : '-100') + "%";
+    toPage.style.left = '0%';
+
+    timeoutID = setTimeout(function () {
+      removeClass(fromPage, 'selected'); //Hide the fromPage
+    }, 1000);
+  }
+
+}, false);
