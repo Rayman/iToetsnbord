9367222908e28c36d06d07b7c786288c8a711837
Ramon Wijnands <ramon.wijnands007@gmail.com>
2010-04-04 15:02:47 +0200

    Add JSON.parse to framework

--- scripts/framework.js
+++ scripts/framework.js
@@ -192,3 +192,39 @@ var Request = new Class({
     this.xhr.send();
   }
 });
+
+var JSON = JSON || {};
+
+//JSON.parse is a modified code from http://www.json.org/json2.js
+if (typeof JSON.parse !== 'function') {
+  JSON.parse = function (text) {
+      var j;
+      var cx = /[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+
+      //cast to string
+      text = String(text);
+
+      //some unicode stuff
+      cx.lastIndex = 0;
+      if (cx.test(text)) {
+          text = text.replace(cx, function (a) {
+              return '\\u' +
+                  ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+          });
+      }
+
+
+      if (/^[\],:{}\s]*$/
+        .test(
+          text
+            .replace(/\\(?:["\\\/bfnrt]|u[0-9a-fA-F]{4})/g, '@')
+            .replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']')
+            .replace(/(?:^|:|,)(?:\s*\[)+/g, '')
+        )
+      ) {
+          j = eval('(' + text + ')');
+          return j;
+      }
+      throw new SyntaxError('JSON.parse');
+  };
+}
