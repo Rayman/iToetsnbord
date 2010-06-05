828b1a39068961be5c5b5a5ae68212d1cfc350e9
Ramon Wijnands <ramon.wijnands007@gmail.com>
2010-06-05 22:27:52 +0200

    Improvements on the documentation of framework.js

--- scripts/framework.js
+++ scripts/framework.js
@@ -1,4 +1,29 @@
 /**
+ * Copyright (c) <2010> <Ramon Wijnands>
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/**
  * Gets an element by its id from the document
  * @param {string} id The id of the element
  */
@@ -24,6 +49,11 @@ var log = function () {
   }
 };
 
+/**
+ * Copy all propperties from the extended object to the original
+ * @param {object} original The object to copy the properties TO
+ * @param {object} extended The object to copy the properties FROM
+ */
 function $extend(original, extended) {
 	for (var key in (extended || {})) {
     original[key] = extended[key];
@@ -31,6 +61,9 @@ function $extend(original, extended) {
 	return original;
 }
 
+/**
+ * Try each argument (as a function) until one succeeds
+ */
 function $try() {
 	for (var i = 0, l = arguments.length; i < l; i++) {
 		try {
@@ -40,6 +73,10 @@ function $try() {
 	return null;
 }
 
+/**
+ * Determine the type of the object
+ * @return A string with the type of the object
+ */
 function $type(obj) {
 	if (obj == undefined) {
     return false;
@@ -72,7 +109,10 @@ function $type(obj) {
 	return t;
 }
 
-//Make array
+/**
+ * Convert the iterable object to a new array
+ * @param {object} iterable An object that is iterable such as a NodeList or an array
+ */
 function $A(iterable) {
 	if (iterable.item) {
 		var l = iterable.length, array = new Array(l);
@@ -84,6 +124,9 @@ function $A(iterable) {
 	return Array.prototype.slice.call(iterable);
 }
 
+/**
+ * Bind the 'this' variable of the function to the desired object
+ */
 Function.prototype.bind = function (obj) {
   var that = this;
   return function () {
@@ -91,6 +134,10 @@ Function.prototype.bind = function (obj) {
   };
 };
 
+/**
+ * Call the function fn on each object in the array
+ * @param {object=} bind An optional object to bind the function to
+ */
 Array.prototype.each = function (fn, bind) {
   for (var i = 0, l = this.length; i < l; i++) {
     fn.call(bind, this[i], i, this);
@@ -109,34 +156,58 @@ function $each(obj, fn, bind) {
   }
 }
 
+/**
+ * Replace all multiple white space in the string to one space
+ */
 String.prototype.clean = function () {
   return this.replace(/\s+/g, ' ').trim();
 };
 
+/**
+ * An empty function
+ */
 function $empty() { }
 
+/**
+ * Removes all whitespace at the beginning and at the end of the string
+ */
 String.prototype.trim = function () {
   return this.replace(/^\s+|\s+$/g, '');
 };
 
+/**
+ * Checks if the element has a class
+ */
 function hasClass(element, className) {
   return (' ' + element.className + ' ').indexOf(' ' + className + ' ') > -1;
 }
 
+/**
+ * Add a class to an element
+ */
 function addClass(element, className) {
   if (!hasClass(element, className)) {
     element.className = (element.className + ' ' + className).clean();
   }
 }
 
+/**
+ * Remove a class from an element
+ */
 function removeClass(element, className) {
   element.className = element.className.replace(new RegExp('(^|\\s)' + className + '(?:\\s|$)'), '$1');
 }
 
+/**
+ * If toggle a class on an element
+ */
 function toggleClass(element, className) {
   hasClass(element, className) ? removeClass(element, className) : addClass(element, className);
 }
 
+/**
+ * Remove all childNodes from an element
+ */
 function empty(element) {
   $each($A(element.childNodes), function (node) {
     empty(node);
@@ -144,6 +215,9 @@ function empty(element) {
   });
 }
 
+/**
+ * Get an array with all the childNodes from an element
+ */
 function getChildren(element) {
   var el = element.firstChild;
 	var elements = [];
@@ -157,7 +231,7 @@ function getChildren(element) {
 }
 
 /**
- * A request implementation
+ * A simple XMLHttpRequest implementation
  * @constructor
  */
 function Request(options) {
@@ -197,17 +271,20 @@ Request.prototype = {
   }
 };
 
-
+/**
+ * Convert a json string to an object
+ */
 function json_parse(jsonText) {
   //cast to string
   var text = String(jsonText);
   return eval('(' + text + ')');
 }
 
-/*
-* I got this function from: www.albionresearch.com
-* Their copyright notice is too long to include here :P
-*/
+/**
+ * URLDecode a string
+ * I got this function from: www.albionresearch.com
+ * Their copyright notice is too long to include here :P
+ */
 function URLDecode(text) {
   // Replace + with ' '
   // Replace %xx with equivalent character
@@ -242,6 +319,9 @@ function URLDecode(text) {
   return plaintext;
 }
 
+/**
+ * URLEncode a string
+ */
 function URLEncode(text) {
 	// The Javascript escape and unescape functions do not correspond
 	// with what browsers actually do...
@@ -276,14 +356,12 @@ function URLEncode(text) {
 };
 
 /**
- * Assets.js
- * Provides methods to dynamically load JavaScript, CSS, and Image files into the document.
+ * Asset provides methods to dynamically load JavaScript, CSS, and Image files into the document.
  * license: MIT-style license
  * author: Valerio Proietti
  *
  * The script has been heavily modified
  */
-
 var Asset = {
 
 	javascript: function (source) {
